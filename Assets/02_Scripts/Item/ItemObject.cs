eb68591524fccf49c7bc0614ee79903bff8b9ef0
Cheol <ausqkf301@naver.com>
2025-11-17 16:49:46 +0900

    feat : ItemTestScene, Tree Prefab, Object Interaction 추가

---
+++ Assets/02_Scripts/Item/ItemObject.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemData data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        // throw new System.NotImplementedException();
+        // 플레이어 인벤토리 달면 추가
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/02_Scripts/Item/ResourceObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceObject : MonoBehaviour//, IInteractable
+{
+    public ItemData giveItem;
+    public int quantityPerHithit;
+    public int capacity;
+
+    //public float testTime;
+    //private void Update()
+    //{
+    //    testTime = Time.deltaTime;
+    //    if (testTime > 0)
+    //    {
+    //        OnInteract();
+    //        testTime = 0;
+    //    }
+    //}
+
+    public void OnInteract(Vector3 hitPoint, Vector3 hitNomal)
+    {
+        for (int i = 0; i < quantityPerHithit; i++)
+        {
+            if (capacity <= 0) break;
+            {
+                capacity-=1;
+                Instantiate(giveItem.dropPrefab, Vector3.up + hitPoint, Quaternion.LookRotation(hitNomal, Vector3.up));
+            }
+        }
+        Destroy(this.gameObject);
+    }
+}
