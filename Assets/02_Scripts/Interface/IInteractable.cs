1805e7c5f0c04888a9f16cbb9d8889ea8cddf16c
SuraJeon <sura6856@naver.com>
2025-11-17 11:00:40 +0900

    chore: Interactable 내 함수 OnIntaract으로 이름 변경

--- Assets/02_Scripts/Interface/IInteractable.cs
+++ Assets/02_Scripts/Interface/IInteractable.cs
@@ -6,10 +6,10 @@ public interface IInteractable
     /// 플레이어가 해당 오브젝트에 타게팅한 후,
     /// 상호작용 버튼 눌렀을 때 작동할 내용을 구현해 주세요! 
     /// </summary>
-    public void Interaction();
+    public void OnInteract();
     
     // 타게팅이 됐을 때, 벗어났을 때 할 동작 고려
     // 지금은 있으면 선언하기 귀찮으니 나중에 필요하면 추가 합시다~!
-    // public void InteractionRangeEnter();
-    // public void InteractionRangeExit();
+    // public void OnInteractionRangeEnter();
+    // public void OnInteractionRangeExit();
 }
