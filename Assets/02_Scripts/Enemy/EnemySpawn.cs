e1b50c3636c8b9aff6333597eb8098f7fc235efa
songduckhee <vhaqjvl1@gmail.com>
2025-11-17 19:54:13 +0900

    feat: 스폰 기능 생성

---
+++ Assets/02_Scripts/Enemy/EnemySpawn.cs
@@ -0,0 +1,44 @@
+﻿using _02_Scripts.Core.Managers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EnemySpawn : MonoBehaviour
+{
+    public GameObject SpawnMonster;
+	public Transform[] SpawnPoint;
+	public bool isNight;
+
+	private void Start()
+	{
+		GameManager.Instance.OnNightStart += SpawnMon;
+	}
+
+	private void Update()
+	{
+		if (Input.GetKeyDown("a"))
+		{
+			SpawnMon();
+		}
+	}
+
+
+	[ContextMenu("Test")]
+	public void SpawnMon()
+	{
+		for(int i = 0;  i < 5; i++)
+		{
+			Invoke("Spawn",5f);
+		}
+	}
+
+	public void Spawn()
+	{
+		int o = Random.Range(0, SpawnPoint.Length);
+		Instantiate(SpawnMonster, SpawnPoint[o]);
+	}
+
+	 
+}
