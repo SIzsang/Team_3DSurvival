efe148e402ada74020ffa9fec8257a270cdc56c0
Cheol <ausqkf301@naver.com>
2025-11-18 16:59:47 +0900

    feat : House 생성, 물 상호작용(인벤토리 연동 후) 추가

--- Assets/02_Scripts/Item/HouseObject.cs
+++ Assets/02_Scripts/Item/HouseObject.cs
@@ -1,12 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class HouseObject : MonoBehaviour, IInteractable
 {
     public ItemData houseData;
+
+    public GameObject houseObject;
+
+    public Transform housePoint;
+
     public void OnInteract()
     {
-        Instantiate(houseData.dropPrefab /* 생성 위치 */ );
+        Instantiate(houseObject, housePoint);
     }
+
+    //void Update()
+    //{
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        Instantiate(houseObject, housePoint);
+    //    }
+    //}
 }
---
+++ Assets/02_Scripts/Item/WaterObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterObject : MonoBehaviour, ICombatable
+{
+    //public ItemData data;
+
+    //public string GetInteractPrompt()
+    //{
+    //    string str = $"{data.displayName}\n{data.description}"; // player.~~~
+    //    return str;
+    //}
+    public void OnInteract()
+    {
+        // 상호작용시 물 아이템 획득
+    }
+}
