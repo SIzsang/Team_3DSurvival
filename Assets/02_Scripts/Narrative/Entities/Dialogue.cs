3f7989ef054bfb6c955ce45bb2cbfb662eb73fe1
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2025-11-18 10:21:11 +0900

    feat : 문자열 기반 대화창 호출 메서드추가

--- Assets/02_Scripts/Narrative/Entities/Dialogue.cs
+++ Assets/02_Scripts/Narrative/Entities/Dialogue.cs
@@ -6,16 +6,40 @@ namespace _02_Scripts.Narrative.Entities
     {
         private readonly DialogueData _data;
         private int _currentLineIndex = -1;
+        private DialogueLine _singleSentence;
 
-        public bool IsFinished => _currentLineIndex >= _data.dialogueLines.Count - 1;
+        // public bool IsFinished => _currentLineIndex >= _data.dialogueLines.Count - 1;
 
         public Dialogue(DialogueData dialogueData)
         {
             _data = dialogueData;
         }
 
+        public Dialogue(string singleSentence)
+        {
+            _singleSentence = new DialogueLine();
+            _singleSentence.text = singleSentence;
+        }
+
+        public bool IsFinished()
+        {
+            if (_currentLineIndex != -1)
+            {
+                return _currentLineIndex >= _data.dialogueLines.Count - 1;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+
         public DialogueLine GetNextLine()
         {
+            if (_data == null && _singleSentence != null)
+            {
+                return _singleSentence;
+            }
             _currentLineIndex++;
 
             if (_currentLineIndex >= _data.dialogueLines.Count)
