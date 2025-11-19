38a98934269a7ef7a3db36eaa472793e76d1503b
songduckhee <vhaqjvl1@gmail.com>
2025-11-19 15:58:40 +0900

    feat : Health바 업데이트 구현, 스폰몬스터리스트구현과 리스트내 몬스터 destroy 매서드 구현

--- Assets/02_Scripts/Enemy/EnemyHealthBar.cs
+++ Assets/02_Scripts/Enemy/EnemyHealthBar.cs
@@ -4,7 +4,24 @@ using UnityEngine;
 
 public class EnemyHealthBar : MonoBehaviour
 {
-	[SerializeField] private GameObject Health;
+	public GameObject Health;
 
-	public GameObject Enemy; 
+	public Enemy Enemy;
+	float MaxHealth;
+	float CurrentHealth;
+
+	private void Start()
+	{
+		MaxHealth = Enemy.health;
+	}
+	private void Update()
+	{
+		UpdateHealth();
+	}
+	void UpdateHealth()
+	{
+		CurrentHealth = Enemy.health;
+		float healthScale = CurrentHealth / MaxHealth;
+		Health.transform.localScale = new Vector3(healthScale, Health.transform.localScale.y, Health.transform.localScale.z);
+	}
 }
