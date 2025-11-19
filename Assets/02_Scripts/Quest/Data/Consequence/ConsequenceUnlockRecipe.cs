e4497f06aab535d65cd175904704bc844672c937
Cheol <ausqkf301@naver.com>
2025-11-19 14:07:37 +0900

    fix : 오탈자 수정

--- Assets/02_Scripts/Quest/Data/Consequence/ConsequenceUnlockRecipe.cs
+++ Assets/02_Scripts/Quest/Data/Consequence/ConsequenceUnlockRecipe.cs
@@ -1,11 +1,11 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace _02_Scripts.Quest.Data.Consequence
 {
     [CreateAssetMenu(fileName = "New ConsequenceUnlockRecipe", menuName = "Scriptable Objects/Quest/Consequence/UnlockRecipe")]
     public class ConsequenceUnlockRecipe : QuestConsequence
     {
-        [SerializeField] ResipeData resipe;
+        [SerializeField] RecipeData resipe;
         public override void Consequence()
         {
 
